bfeac9b3253f494810c1469f93193d3747b3e8d3
rbnscr <robin.schroeder94@googlemail.com>
2023-09-20 21:52:39 +0200

    removed obsolete code

--- xyzMerge.py
+++ xyzMerge.py
@@ -1,4 +1,3 @@
-import os
 import pandas as pd
 from alive_progress import alive_bar
 import numpy as np
@@ -8,75 +7,7 @@ import tkinter.filedialog as fd
 
 from matplotlib.path import Path
 
-def handle_inputs():
-    coordinates_keys = ["x0", "x1", "y0", "y1"]
-    coordinates = dict.fromkeys(coordinates_keys)
-    for key in coordinates.keys():
-        validity_check = False
-        while validity_check is False:
-            print(f"Select {key}:")
-            coordinate_input = input()
-            try:
-                coordinate_input = int(coordinate_input)
-                coordinates[key] = coordinate_input
-                validity_check = True
-            except ValueError:
-                print("Input has to be a number.")
-                validity_check = False
-    if coordinates["x0"] > coordinates["x1"]:
-        x0_ = coordinates["x1"]
-        coordinates["x1"] = coordinates["x0"]
-        coordinates["x0"] = x0_
-
-    if coordinates["y0"] > coordinates["y1"]:
-        y0_ = coordinates["y1"]
-        coordinates["y1"] = coordinates["y0"]
-        coordinates["y0"] = y0_
-
-    return coordinates
-
-
-def get_xyz_files():
-    xyz_files = []
-    for dirpath, dirnames, filenames in os.walk("."):
-        for filename in [f for f in filenames if f.endswith(".xyz")]:
-            if dirpath != ".":
-                xyz_files.append(os.path.join(dirpath, filename))
-    print("Files found:")
-    print(*xyz_files, sep="\n")
-    print("Continue? (Y/n)")
-    decider = input().capitalize()
-    print(decider)
-    if decider == "Y" or decider == "":
-        return xyz_files
-    else:
-        exit()
-
-
-def process_files(target_coordiantes, xyz_files):
-    df_all = pd.DataFrame(columns=["x", "y", "z"])
-    with alive_bar(len(xyz_files),dual_line=True) as bar:
-        for file in xyz_files:
-            bar.text = f"-> Processing file: {file}, please wait..."
-            df = pd.read_table(
-                file, delim_whitespace=True, header=None, names=["x", "y", "z"]
-            )
-            df = df.loc[
-                (df["x"] >= target_coordiantes["x0"])
-                & (df["x"] <= target_coordiantes["x1"])
-                & (df["y"] >= target_coordiantes["y0"])
-                & (df["y"] <= target_coordiantes["y1"])
-            ]
-            df_all = pd.concat([df_all, df])
-            if df.empty:
-                print(f"No matching coordinates found in file: {file}")
-            else:
-                print(f"Matching coordinates found in file: {file}")
-            bar()
-    return df_all
-
 def process_files_poly(target_coordiantes, xyz_files):
-    # p = Path([(target_coordiantes["x0"],target_coordiantes["y0"]),(target_coordiantes["x0"],target_coordiantes["y1"]),(target_coordiantes["x1"],target_coordiantes["y1"]),(target_coordiantes["x1"],target_coordiantes["y0"])])
     p = Path(target_coordiantes)
     print(p)
     df_all = pd.DataFrame(columns=["x", "y", "z"])
@@ -99,8 +30,6 @@ def process_files_poly(target_coordiantes, xyz_files):
 
 
 def save_xyz(file: pd.DataFrame):
-    # print("Enter filename...")
-    # filename = input()
     save_filename = fd.asksaveasfilename(defaultextension='.xyz')
     if not save_filename:
         print("Filename empty... Abort.")
@@ -108,8 +37,7 @@ def save_xyz(file: pd.DataFrame):
     if not save_filename.endswith('.xyz'):
         save_filename = save_filename + '.xyz'
     file.to_csv(save_filename, sep=" ", header=None, index=False)
-    # file.to_csv(f"{filename}.xyz", sep=" ", header=None, index=False)
-    # print(f"File saved: {os.path.join(os.path.dirname(os.path.realpath(__file__)),filename)}.xyz")
+
 
 def end_loop():
     print("Re-run? (Y)")
